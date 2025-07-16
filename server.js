b2a628b1d5adc527704b4cb8e9ca12829273fc6f
“Abhishek <abhishekpipriye2507@gmail.com>
2025-07-16 23:26:29 +0530

    fix: implement request handling and correct log message in server setup

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const http = require('http') 
 
-const server = http.createServer()
+const server = http.createServer((req,res)=>{
+    res.end("Hello , world!")
+
+})
 
 server.listen(3000,()=>{
-    console.log('server is unning on port 3000')
+    console.log('server is running on port 3000')
 })
