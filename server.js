96ff2c41ed344bd7e731a9f89130d68475a56149
“Abhishek <abhishekpipriye2507@gmail.com>
2025-07-18 14:04:32 +0530

    feat: add functionality to store and return notes

--- server.js
+++ server.js
@@ -2,10 +2,18 @@ const express = require('express')
 
 const app = express()
 
+
+const notes = []
+
 // notes => title & description 
 app.use(express.json())
 app.post('/notes', (req, res) => {
     console.log(req.body)
+    notes.push(req.body)
+    res.json({
+        message:"Note added successfully",
+        notes:notes
+    })
 })
 
 
